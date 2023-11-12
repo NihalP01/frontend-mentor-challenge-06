b328203d6e5bd7703d5267fb958f67dad14656e0
Nihalp01 <nihalburagohain02@gmail.com>
2023-11-12 15:16:30 +0530

    new changes

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const btnMinus = document.getElementById('btn-minus');
 const countText = document.getElementById('count-text');
 const cartBadgeCount = document.getElementById('cart-badge');
 const addToCartButton = document.getElementById('add-to-cart');
+const cartDetails = document.getElementsByClassName('cart-details');
+const cartButton = document.getElementById('btn-cart');
 
 let count = 0;
+
+updateCartBadgeVisibility();
 
 btnPlus.addEventListener('click', () => {
   if (count < 10) {
@@ -21,7 +25,18 @@ btnMinus.addEventListener('click', () => {
 });
 
 addToCartButton.addEventListener('click', () => {
-  cartBadgeCount.innerText = count
+  cartBadgeCount.innerText = count;
+  updateCartBadgeVisibility();
 });
 
+function updateCartBadgeVisibility() {
+  if (count < 1) {
+    cartBadgeCount.style.display = 'none';
+  } else {
+    cartBadgeCount.style.display = 'flex';
+  }
+}
 
+cartButton.addEventListener('click', () => {
+  
+});
